20fe1d4795ee22ce1df1258c53d969210cb413bb
oleksiiKAV <oleksii.kav@gmail.com>
2023-07-04 13:52:54 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { lazy } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { GlobalStyle } from '../styles/GlobalStyle';
+import SharedLayout from './SharedLayout/SharedLayout';
+import { ScrollToTopArrw } from './ScrollToTop/ScrollToTop';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = lazy(() => import('../pages/Home/Home'));
+const Tweets = lazy(() => import('../pages/Tweets/Tweets'));
 
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
-
-export default App
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/tweets" element={<Tweets />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+      <ScrollToTopArrw />
+      <GlobalStyle />
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { App } from "./components/App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Tweets-vite">
+    <BrowserRouter basename="/Tweets-Follow">
       <App />
     </BrowserRouter>
   </React.StrictMode>
