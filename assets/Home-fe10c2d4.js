350f438ac78c1df3df3460f8d16c1f8f782c3617
oleksiiKAV <oleksiiKAV@users.noreply.github.com>
2023-07-04 12:16:18 +0000

    deploy: 078cbc227a5b3009f6e5caba71ce433f146c91e2

---
+++ assets/Home-fe10c2d4.js
@@ -0,0 +1,27 @@
+import{s as i,j as t}from"./index-0325821d.js";import{l as e}from"./logo-0fe2b947.js";const o=i.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 70vh;
+  background-image: url(${e});
+  background-repeat: repeat;
+  background-size: 200px;
+  background-position: 10px 10px;
+`,n=i.img`
+border-radius:50%;
+width: 10%;
+  height: 10%;
+  animation: scaleAnimation 2s ease-in-out;
+
+  @keyframes scaleAnimation {
+    0% {
+      width: 10%;
+      height: 10%;
+    }
+    100% {
+      width: 30%;
+      height: 30%;
+    }    
+  }
+`,s="/Tweets-Follow/assets/tw_logo-adca8db2.gif";function d(){return t.jsxs(o,{children:[t.jsx("h1",{hidden:!0,children:"GOIT REACT TECH"}),t.jsx(n,{src:s,width:"10%",alt:"twitlogo"})]})}export{d as default};
